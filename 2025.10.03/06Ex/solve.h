e5dc17c7201249be27f229ca3989d148ddc72d1b
AZEN-SGG <rascatov.alex@gmail.com>
2025-10-03 04:59:42 +0300

    Task 6 is done!

---
+++ 2025.10.03/06Ex/solve.h
@@ -0,0 +1,12 @@
+#ifndef SOLVE_H
+#define SOLVE_H
+
+#include "student.h"
+
+#include <utility>
+
+void t6_solve(student *arr, int n);
+int find(student *arr, int n, student& x);
+void append(student *arr, int n, int index);
+
+#endif
