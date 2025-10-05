830626f28e2f235fa7d34e96f620b99d1ac2f4f5
AZEN-SGG <rascatov.alex@gmail.com>
2025-10-05 11:03:21 +0300

    Task 1 is done!

---
+++ 2025.10.10/01Ex/solve.cpp
@@ -0,0 +1,20 @@
+#include "solve.h"
+
+int t1_solve (student *arr, int n)
+{
+	int del_num = 0;
+	int is_empty = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (arr[is_empty ? i - 1 - del_num : i - 1] > arr[i]) {
+			del_num++;
+			is_empty = 0;
+		} else if (del_num) {
+			arr[i-del_num] = (student &&)arr[i];
+			is_empty = 1;
+		}
+	}
+
+	return n - del_num;
+}
+
---
+++ 2025.10.10/01Ex/solve.h
@@ -0,0 +1,10 @@
+#ifndef SOLVE_H
+#define SOLVE_H
+
+#include "student.h"
+
+#include <utility>
+
+int t1_solve (student *arr, int n);
+
+#endif
