75d0ec76855baaa0fa6e7ecc33c2bcf86d51a652
AZEN-SGG <rascatov.alex@gmail.com>
2025-11-12 16:55:11 +0300

    Started Task 2

---
+++ 2025.11.14/02Ex/solve.cpp
@@ -0,0 +1,49 @@
+#include "tree.h"
+
+void tree::t1_solve ()
+{
+	family extr;
+
+	for (tree_node *prev = nullptr, *curr = root ; (curr != nullptr) ; prev = curr, curr = curr->right)
+	{
+		extr = find_max_subtree(curr);
+		
+		if (extr.parent != nullptr)
+		{
+			switch (extr.dir)
+			{
+				case 0:
+					extr.parent->left = curr;
+					break;
+				case 1:
+					extr.parent->right = curr;
+					break;
+			}
+
+			extr.parent = curr->left; // Like temp
+			curr->left = extr.child->left;
+			extr.child->left = extr.parent;
+
+			extr.parent = curr->right; // Again
+			curr->right = extr.child->right;
+			extr.child->right = extr.parent;
+
+			if (prev == nullptr) // if curr = root
+				root = extr.child;
+			else
+				prev->right = extr.child;
+
+			curr = extr.child;
+		} // else - root, not needed to change smth
+
+
+		if (curr->left != nullptr)
+		{
+			tree temp;
+			temp.root = curr->left;
+			temp.t1_solve();
+			curr->left = temp.root;
+			temp.root = nullptr;
+		}
+	}
+}
