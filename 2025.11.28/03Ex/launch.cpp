12bd8358069f0470ae193eeb776614c351d441e5
AZEN-SGG <rascatov.alex@gmail.com>
2025-11-27 17:54:07 +0300

    Task 3 is done!

---
+++ 2025.11.28/03Ex/launch.cpp
@@ -0,0 +1,70 @@
+#include "launch.h"
+
+template <typename T>
+int launch (char * path, char * filename, int r)
+{
+	tree<T> *olha = new tree<T>;
+    if (!olha)
+    {
+		fprintf (stderr, "Error: MEMORY -> tree is nullptr\n");
+        return 2;
+    }
+
+	io_status ret = olha->read_file(filename);
+
+	do {
+		switch (ret)
+		{
+			case io_status::success:
+				continue;
+			case io_status::open:
+				fprintf (stderr, "Error: Cannot open %s\n", filename);
+				break;
+			case io_status::format:
+				fprintf (stderr, "Error: Wrong format of file %s\n", filename);
+				break;
+			case io_status::eof:
+				fprintf (stderr, "Error: End of file %s\n", filename);
+				break;
+			case io_status::memory:
+				fprintf (stderr, "Error: MEMORY\n");
+				break;
+		}
+
+		delete olha;
+
+		return 3;
+	} while (0);
+
+	fprintf (stdout, "Original tree:\n");
+	olha->print(r);
+
+    int (tree<T>::*solves[])() const = {
+        &tree<T>::t1_solve,
+        &tree<T>::t2_solve,
+        &tree<T>::t3_solve,
+    };
+    int len = sizeof(solves) / sizeof(solves[0]);
+
+	double t = 0;
+    int res = 0;
+    for (int i = 0 ; i < len ; ++i)
+    {
+	    t = clock();
+        res = (olha->*solves[i])();
+        t = (clock() - t) / CLOCKS_PER_SEC;
+
+	    fprintf(stdout, "%s : Task = %d Result = %d Elapsed = %.2f\n", path, i + 1, res, t);
+    }
+
+	fprintf (stdout, "Modified tree:\n");
+	olha->print(r);
+		
+	delete olha;
+
+	return 0;
+}
+
+template int launch<student>(char *, char *, int);
+template int launch< list2<student> >(char *, char *, int);
+
---
+++ 2025.11.28/03Ex/student.h
@@ -0,0 +1,151 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "io_status.h"
+
+#include <cstring>
+#include <cstdio>
+
+#define LEN 1234
+#define STUDENT_NAME "Student"
+
+class student
+{
+	private:
+		char *name = nullptr;
+		int value = 0;
+	public:
+		student () = default;
+		student (const student& x) = delete;
+		student (const char *n, int v) { init (n, v); }
+		student (student&& x)
+		{
+			name = x.name; x.name = nullptr;
+			value = x.value; x.value = 0;
+		}
+
+		~student ()	{ erase ();	}
+
+		student& operator= (const student& x) = delete;
+		student& operator= (student&& x)
+		{
+			if (this == &x)
+				return *this;
+			erase ();
+			name = x.name; x.name = nullptr;
+			value = x.value; x.value = 0;
+			return *this;
+		}
+
+		int get_length () const { return ((name != nullptr) ? 1 : 0); }
+		int is_empty () const {	return !get_length(); }
+
+		void print (FILE *fp = stdout, int level = 0) const
+		{
+            int spaces = level << 1;
+			for (int i = 0 ; i < spaces ; i++)
+				fprintf(fp, " ");
+			fprintf (fp, "%s %d\n", name, value);
+		}
+		
+		io_status read (FILE *fp)
+		{
+			char n[LEN];
+			int v;
+
+			if (fscanf (fp, "%s%d", n, &v) != 2)
+			{
+				if (!feof (fp))
+					return io_status::format;
+				return io_status::eof;
+			}
+			erase ();
+			return init (n, v);
+		}
+		void init_f (const int s, const int n, const int i)
+		{
+			erase ();
+			name = new char[sizeof(STUDENT_NAME)];
+			strcpy(name, STUDENT_NAME);
+
+			switch (s)
+			{
+				case 1:
+					value = i;
+					break;
+				case 2: 
+					value = n - i;
+					break;
+				case 3:
+					value = i >> 1;
+					break;
+				case 4:
+					value = n - (i >> 1);
+					break;
+			}
+		}
+
+		int operator> (const student& x) const
+		{
+			return (cmp (x) > 0 ? 1 : 0);
+		}
+		int operator< (const student& x) const
+		{
+			return (cmp (x) < 0 ? 1 : 0);
+		}
+		int operator>= (const student& x) const
+		{
+			return (cmp (x) >= 0 ? 1 : 0);
+		}
+		int operator<= (const student& x) const
+		{
+			return (cmp (x) <= 0 ? 1 : 0);
+		}
+		int operator== (const student& x) const
+		{
+			return (cmp (x) == 0 ? 1 : 0);
+		}
+	private:
+		io_status init (const char *n, int v)
+		{
+			value = v;
+			if (n != nullptr)
+			{
+				size_t len = strlen (n);
+				name = new char [len + 1];
+				if (name != nullptr)
+				{
+					for (size_t i = 0; i <= len; i++)
+						name[i] = n[i];
+				} else
+					return io_status::memory;
+			} else
+				name = nullptr;
+			return io_status::success;
+		}
+		void erase ()
+		{
+			value = 0;
+			if (name != nullptr)
+			{
+				delete[] name;
+				name = nullptr;
+			}
+		}
+		int cmp (const student& x) const
+		{
+			if (name == nullptr)
+			{
+				if (x.name != nullptr)
+					return -1;
+				return value - x.value;
+			} if (x.name == nullptr)
+				return 1;
+			int res = strcmp (name, x.name);
+			if (res)
+				return res;
+			return value - x.value;
+		}
+};
+
+#endif // student_h
