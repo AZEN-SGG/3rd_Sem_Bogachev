1103b0c1d713f174cb773a1dbb7da1c2eab0e252
AZEN-SGG <rascatov.alex@gmail.com>
2025-11-30 13:10:22 +0300

    I think, that template is already, but i'm not sure...

---
+++ 2025.12.05/01Ex/launch.h
@@ -0,0 +1,13 @@
+#ifndef LAUNCH_H
+#define LAUNCH_H
+
+#include "io_status.h"
+#include "b_tree.h"
+
+#include <ctime>
+#include <new>
+
+template <typename T>
+int launch (char * path, char * filename, const int m, const int k, int r = 10);
+
+#endif // LAUNCH_H
---
+++ 2025.12.05/01Ex/main.cpp
@@ -0,0 +1,27 @@
+#include "student.h"
+#include "launch.h"
+
+#include "b_tree.h"
+#include "t1_solve.h"
+
+#include <cstdio>
+
+int main(int argc, char *argv[])
+{
+	/* ./a01.out m r k filename */
+	int m, r, k, res;
+
+	if (
+		!(argc == 5
+		&& sscanf(argv[1], "%d", &m) == 1 && (m > 0)
+		&& sscanf(argv[2], "%d", &r) == 1
+		&& sscanf(argv[3], "%d", &k) == 1
+	)) {
+		printf("Usage %s m r k filename\n", argv[0]);
+		return 1;
+	}
+
+	res = launch<student>(argv[0], argv[2], m, k, r);
+
+	return res;
+}
