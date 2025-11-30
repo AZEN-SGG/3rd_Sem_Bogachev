70af492e388186edb48a7fbb771a67a562969226
AZEN-SGG <rascatov.alex@gmail.com>
2025-11-30 18:04:55 +0300

    Task 5 is done!

---
+++ 2025.12.05/05Ex/launch.h
@@ -0,0 +1,21 @@
+#ifndef LAUNCH_H
+#define LAUNCH_H
+
+#include "io_status.h"
+#include "student.h"
+
+#include "b_tree.h"
+
+#include "t1_solve.h"
+#include "t2_solve.h"
+#include "t3_solve.h"
+#include "t4_solve.h"
+#include "t5_solve.h"
+
+#include <ctime>
+#include <new>
+
+template <typename T>
+int launch (char * path, char * filename, const int m, const int k, int r = 10);
+
+#endif // LAUNCH_H
